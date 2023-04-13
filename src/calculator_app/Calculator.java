94615f1c657f1a335d8c5da54c8baa9e7ad35bfe
PathumLakshitha <pathum_2019513@fot.ruh.ac.lk>
2023-04-13 22:09:53 +0530

    alignments and design

--- src/calculator_app/Calculator.java
+++ src/calculator_app/Calculator.java
@@ -5,25 +5,25 @@ import javax.swing.*;
 public class Calculator {
     private JPanel Calculator;
     private JTextField txtDisplay;
-    private JButton button1;
+    private JButton ACButton;
     private JButton button2;
-    private JButton button3;
+    private JButton xButton;
     private JButton button4;
     private JButton button5;
     private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a1Button;
+    private JButton a00Button;
     private JButton button11;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
+    private JButton a8Button;
+    private JButton a5Button;
+    private JButton a2Button;
+    private JButton a0Button;
     private JButton button16;
-    private JButton button17;
-    private JButton button18;
-    private JButton button19;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
     private JButton button20;
 
     public static void main(String[] args) {
